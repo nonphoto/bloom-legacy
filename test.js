1f04cc4a6b7b3724559755a459f8efd5c2f055d9
Jonas Luebbers <jonaskluebbers@gmail.com>
2020-08-30 14:34:02 -0400

    Add input test

--- test.js
+++ test.js
@@ -2,6 +2,7 @@ import { h, init } from "./lib/dom.js";
 import S from "https://cdn.skypack.dev/s-js";
 import SArray from "https://cdn.skypack.dev/s-array";
 
+const value = S.data("");
 const todos = SArray([]);
 S(() => {
   console.log(todos());
@@ -23,12 +24,27 @@ const newTree = [
   ),
   h("div", {}, ["b", "c", 3]),
   h("div", { style: { "--value": seconds } }, todos),
+  h("div", {}, [
+    h("input", {
+      type: "text",
+      value,
+      onInput: (event) => {
+        value(event.target.value);
+      },
+    }),
+    h(
+      "button",
+      {
+        onClick: () => {
+          todos.push(value());
+          value("");
+        },
+      },
+      "add"
+    ),
+  ]),
 ];
 
 init(document.querySelector("#app"), newTree);
-
-todos.push("a");
-todos.push("a");
-todos.push("a");
 
 loop();
